c46632d4560305a2088e2be11bea91f8d114f73d
Junsung Lee <jjoons2010@gmail.com>
2026-04-06 03:19:06 +0900

    chore: Landing에 Footer 연결

--- web/src/pages/Landing.tsx
+++ web/src/pages/Landing.tsx
@@ -1,6 +1,7 @@
 import Navbar from '../components/Navbar';
 import Hero from '../components/Hero';
 import Layers from '../components/Layers';
+import Footer from '../components/Footer';
 
 /**
  * Landing page
@@ -14,6 +15,7 @@ export default function Landing() {
         <Hero />
         <Layers />
       </main>
+      <Footer />
     </div>
   );
 }
